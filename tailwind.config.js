c4208afeed6eeac6cf1263d8727d8b2f82638a83
mrdrmitten <rayxmagner1@gmail.com>
2022-04-23 13:45:25 -0500

    Setup: TailwindCss Configs, Fluid Type/LineHeight, Site Header

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+	content: [
+		'./pages/**/*.{js,ts,jsx,tsx}',
+		'./components/**/*.{js,ts,jsx,tsx}',
+	],
+	theme: {
+		fontFamily: {
+			title: ['cortado, hero-new, sans-serif'],
+		},
+		colors: {
+      'primary': '#403D39',
+      'secondary': '#F5EBE0',
+      'accent': '#CCC5B9',
+    },
+		extend: {},
+	},
+	plugins: [],
+}
